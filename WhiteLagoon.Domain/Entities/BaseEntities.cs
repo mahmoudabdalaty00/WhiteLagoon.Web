0be03269362262dabda35057b28500d38ceec39a
mahmoudabdalaty00 <117662685+mahmoudabdalaty00@users.noreply.github.com>
2025-08-20 15:47:33 +0300

    Add project files.

---
+++ WhiteLagoon.Domain/Entities/BaseEntities.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WhiteLagoon.Domain.Entities
+{
+    public class BaseEntities
+    {
+        [Key]
+        public int Id { get; set; }
+        
+
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
---
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WhiteLagoon.Domain.Entities
+{
+    public class Villa : BaseEntities
+    {
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public int Sqft { get; set; }
+        public int Occupancy { get; set; }
+    }
+}
